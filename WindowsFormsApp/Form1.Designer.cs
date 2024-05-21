f7db54ce218c37f9f2a72b7db2225ae19a85f982
Owner <Owner@DESKTOP-2TPH2RF>
2024-05-22 01:07:08 +0530

    Add project files.

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.lblname = new System.Windows.Forms.Label();
+            this.lblpasswoerd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(122, 129);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(75, 23);
+            this.btnlogin.TabIndex = 0;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(235, 129);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(78, 23);
+            this.btncancel.TabIndex = 1;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(235, 39);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 2;
+            this.txtname.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(235, 84);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(100, 20);
+            this.txtpassword.TabIndex = 3;
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(119, 46);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(60, 13);
+            this.lblname.TabIndex = 4;
+            this.lblname.Text = "User Name";
+            this.lblname.Click += new System.EventHandler(this.lblname_Click);
+            // 
+            // lblpasswoerd
+            // 
+            this.lblpasswoerd.AutoSize = true;
+            this.lblpasswoerd.Location = new System.Drawing.Point(119, 91);
+            this.lblpasswoerd.Name = "lblpasswoerd";
+            this.lblpasswoerd.Size = new System.Drawing.Size(53, 13);
+            this.lblpasswoerd.TabIndex = 5;
+            this.lblpasswoerd.Text = "Password";
+            this.lblpasswoerd.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 217);
+            this.Controls.Add(this.lblpasswoerd);
+            this.Controls.Add(this.lblname);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnlogin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.Label lblpasswoerd;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if(txtname.Text== "admin"&&txtpassword.Text== "admin123") 
+            {
+                MessageBox.Show("Login Succesful");
+            }
+            else
+            {
+                MessageBox.Show("Loggin failed");
+            }
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            txtname.Clear();
+            txtpassword.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
